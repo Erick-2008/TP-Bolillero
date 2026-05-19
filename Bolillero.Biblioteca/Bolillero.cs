b1d17306352eaf579949ce925f76cb4e0da34e79
Erick-2008 <eric.aguirreet12d1@gmail.com>
2026-05-19 13:58:01 -0300

    Update Bolillero.cs

--- Bolillero.Biblioteca/Bolillero.cs
+++ Bolillero.Biblioteca/Bolillero.cs
@@ -1,57 +1,53 @@
-using System.Collections.Generic;
-using System.Linq;
+namespace Bolillero.Biblioteca;
 
-namespace Bolillero.Biblioteca
+public class Bolillero
 {
-    public class Bolillero
+    public List<int> Adentro { get; set; } = [];
+    public List<int> Afuera { get; set; } = [];
+    public IAzar Azar { get; set; }
+
+    public Bolillero(int cantidadBolillas, IAzar azar)
     {
-        public List<int> Adentro { get; set; } = new List<int>();
-        public List<int> Afuera { get; set; } = new List<int>();
-        public IAzar Azar { get; set; }
+        for (int i = 0; i < cantidadBolillas; Adentro.Add(i++)) ;
+        Azar = azar;
+    }
 
-        public Bolillero(int cantidadBolillas, IAzar azar)
-        {
-            for (int i = 0; i < cantidadBolillas; i++) Adentro.Add(i);
-            Azar = azar;
-        }
+    public int SacarBolilla()
+    {
+        int indice = Azar.SacarIndice(Adentro.Count);
+        int bolilla = Adentro[indice];
+        Adentro.RemoveAt(indice);
+        Afuera.Add(bolilla);
+        return bolilla;
+    }
 
-        public int SacarBolilla()
-        {
-            int indice = Azar.SacarIndice(Adentro.Count);
-            int bolilla = Adentro[indice];
-            Adentro.RemoveAt(indice);
-            Afuera.Add(bolilla);
-            return bolilla;
-        }
+    public void ReIngresar()
+    {
+        Adentro.AddRange(Afuera);
+        Afuera.Clear();
+    }
 
-        public void ReIngresar()
+    public bool Jugar(List<int> jugada)
+    {
+        foreach (int bolillaEsperada in jugada)
         {
-            Adentro.AddRange(Afuera);
-            Afuera.Clear();
-        }
-
-        public bool Jugar(List<int> jugada)
-        {
-            foreach (int bolillaEsperada in jugada)
+            if (SacarBolilla() != bolillaEsperada)
             {
-                if (SacarBolilla() != bolillaEsperada)
-                {
-                    ReIngresar(); // Se restauran para la próxima jugada
-                    return false;
-                }
+                ReIngresar(); // Se restauran para la próxima jugada
+                return false;
             }
-            ReIngresar();
-            return true;
         }
+        ReIngresar();
+        return true;
+    }
 
-        public int JugarNVeces(List<int> jugada, int cantidadVeces)
+    public int JugarNVeces(List<int> jugada, int cantidadVeces)
+    {
+        int victorias = 0;
+        for (int i = 0; i < cantidadVeces; i++)
         {
-            int victorias = 0;
-            for (int i = 0; i < cantidadVeces; i++)
-            {
-                if (Jugar(jugada)) victorias++;
-            }
-            return victorias;
+            if (Jugar(jugada)) victorias++;
         }
+        return victorias;
     }
 }
